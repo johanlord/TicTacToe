dd8120b11f6fc9d2badbec44a08030bf9b72a3a6
Milos Lazarevic <milaza84@gmail.com>
2018-11-28 18:13:21 +0100

    First Commit - Insert Solution

---
+++ TicTakToe/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace TicTakToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.x = new System.Windows.Forms.Label();
+            this.o = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn9
+            // 
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            resources.ApplyResources(this.btn8, "btn8");
+            this.btn8.Name = "btn8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // x
+            // 
+            resources.ApplyResources(this.x, "x");
+            this.x.Name = "x";
+            // 
+            // o
+            // 
+            resources.ApplyResources(this.o, "o");
+            this.o.Name = "o";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.o);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label x;
+        private System.Windows.Forms.Label o;
+    }
+}
+
---
+++ TicTakToe/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TicTakToe
+{
+    public partial class Form1 : Form
+    {
+        int winx=0,wino=0;
+        int mouseCounter,bt1=3, bt2=3, bt3=3, bt4=3, bt5=3, bt6=3, bt7=3, bt8=3, bt9=3;
+        
+        public static string RootFolder()
+        {
+            DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
+            DirectoryInfo root = di.Parent.Parent;
+            return root.FullName;
+        }
+
+        string backgrPath = Path.Combine(RootFolder(), "blue.jpg");
+        string xPath = Path.Combine(RootFolder(), "iks.png");
+        string oPath = Path.Combine(RootFolder(), "oks.png");
+
+        public Form1()
+        {
+            InitializeComponent();
+            cleargame();
+            foreach (Control c in this.Controls)
+            {
+                c.Click += ClickCounter;
+                
+            }
+        }
+
+        private void ClickCounter(object sender, EventArgs e)
+        {
+            mouseCounter++;
+            x.Text = Convert.ToString(winx);
+            o.Text = Convert.ToString(wino);
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if(mouseCounter % 2 == 0)
+            {          
+                btn1.Image = Image.FromFile(xPath);
+                bt1 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn1.Image = Image.FromFile(oPath);
+                bt1 = 0;
+            }
+            check();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {           
+                btn2.Image = Image.FromFile(xPath);
+                bt2 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn2.Image = Image.FromFile(oPath);
+                bt2 = 0;
+            }
+            check();
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {
+                btn3.Image = Image.FromFile(xPath);
+                bt3 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn3.Image = Image.FromFile(oPath);
+                bt3 = 0;
+            }
+            check();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {             
+                btn4.Image = Image.FromFile(xPath);
+                bt4 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn4.Image = Image.FromFile(oPath);
+                bt4 = 0;
+            }
+            check();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cleargame();
+            mouseCounter = -1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {               
+                btn5.Image = Image.FromFile(xPath);
+                bt5 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn5.Image = Image.FromFile(oPath);
+                bt5 = 0;
+            }
+            check();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {
+                btn6.Image = Image.FromFile(xPath);
+                bt6 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn6.Image = Image.FromFile(oPath);
+                bt6 = 0;
+            }
+            check();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {               
+                btn7.Image = Image.FromFile(xPath);
+                bt7 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn7.Image = Image.FromFile(oPath);
+                bt7 = 0;
+            }
+            check();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {               
+                btn8.Image = Image.FromFile(xPath);
+                bt8 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn8.Image = Image.FromFile(oPath);
+                bt8 = 0;
+            }
+            check();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (mouseCounter % 2 == 0)
+            {               
+                btn9.Image = Image.FromFile(xPath);
+                bt9 = 1;
+            }
+            else if (mouseCounter % 2 == 1)
+            {
+                btn9.Image = Image.FromFile(oPath);
+                bt9 = 0;
+            }
+            check();
+        }
+        private void check()
+        {
+            if (bt1 != 3 && bt2 != 3 && bt3 != 3)
+            {
+
+                if (bt1==0 && bt2==0 && bt3==0)
+                {
+                    MessageBox.Show("O is Winner");
+                    wino++;
+                    play();
+                }
+                else if (bt1 == 1 && bt2 == 1 && bt3 == 1)
+                {
+                    MessageBox.Show("X is Winner");
+                    winx++;
+                    play();
+                }
+            }
+
+            if (bt1 != 3 && bt5 != 3 && bt9 != 3)
+            {
+                if (bt1 == 0 && bt5 == 0 && bt9 == 0)
+                {
+                    MessageBox.Show("O is Winner");
+                    wino++;
+                    play();
+                }
+
+                else if (bt1 == 1 && bt5 == 1 && bt9 == 1)
+                {
+                    MessageBox.Show("X is Winner");
+                    winx++;
+                    play();                    
+                }
+            }
+
+            if (bt1 != 3 && bt4 != 3 && bt7 != 3)
+            {
+                if (bt1 == 0 && bt4 == 0 && bt7 == 0)
+                {
+                    MessageBox.Show("O is Winner");
+                    wino++;
+                    play();
+                }
+
+                else if (bt1 == 1 && bt4 == 1 && bt7 == 1)
+                {
+                    MessageBox.Show("X is Winner");
+                    winx++;
+                    play();
+                }
+            }
+
+            if (bt2 != 3 && bt5 != 3 && bt8 != 3)
+            {
+                if (bt2 == 0 && bt5 == 0 && bt8 == 0)
+                {
+                    MessageBox.Show("O is Winner");
+                    wino++;
+                    play();
+                    
+                }
+
+                else if (bt2 == 1 && bt5 == 1 && bt8 == 1)
+                {
+                    MessageBox.Show("X is Winner");
+                    winx++;
+                    play();                    
+                }
+            }
+            if (bt3 != 3 && bt6 != 3 && bt9 != 3)
+            {
+                if (bt3 == 0 && bt6 == 0 && bt9 == 0)
+                {
+                    MessageBox.Show("O is Winner");
+                    wino++;
+                    play();                   
+                }
+
+                else if (bt3 == 1 && bt6 == 1 && bt9 == 1)
+                {
+                    MessageBox.Show("X is Winner");
+                    winx++;
+                    play();                   
+                }
+            }
+            if (bt3 != 3 && bt5 != 3 && bt7 != 3)
+            {
+                if (bt3 == 0 && bt5 == 0 && bt7 == 0)
+                {
+                    MessageBox.Show("O is Winner");
+                    wino++;
+                    play();
+                    
+                }
+                else if (bt3 == 1 && bt5 == 1 && bt7 == 1)
+                {
+                    MessageBox.Show("X is Winner");
+                    winx++;
+                    play();                    
+                }
+            }
+            if (bt4 != 3 && bt5 != 3 && bt6 != 3)
+            {
+                if (bt4 == 0 && bt5 == 0 && bt6 == 0)
+                {
+                    MessageBox.Show("O is Winner");
+                    wino++;
+                    play();
+                    
+                }
+                else if (bt4 == 0 && bt5 == 0 && bt6 == 0)
+                {
+                    MessageBox.Show("X is Winner");
+                    winx++;
+                    play();                    
+                }
+            }
+            if (bt7 != 3 && bt8 != 3 && bt9 != 3)
+            {
+                if (bt7 == 0 && bt8 == 0 && bt9 == 0)
+                {
+                    MessageBox.Show("O is Winner");
+                    play();
+                    wino++;
+                }
+                else if (bt7 == 1 && bt8 == 1 && bt9 == 1)
+                {
+                    MessageBox.Show("X is Winner");
+                    play();
+                    winx++;
+                }
+            }
+             if (mouseCounter == 8)
+            {
+               
+                MessageBox.Show("It's a Draw......");
+                mouseCounter = -1;
+
+                play();
+            }
+
+
+        }
+        public void cleargame()
+        {
+            bt1 = 3;
+            bt2 = 3;
+            bt9 = 3;
+            bt3 = 3;
+            bt4 = 3;
+            bt5 = 3;
+            bt6 = 3;
+            bt7 = 3;
+            bt8 = 3;
+           
+            // Buttons background at game start
+            btn1.Image = btn2.Image = btn3.Image = btn4.Image = btn5.Image = btn6.Image = btn7.Image = btn8.Image = btn9.Image =
+                Image.FromFile(backgrPath);
+
+        }
+
+        public void play()
+        {
+            cleargame();
+            mouseCounter = -1;
+        }
+
+        public void OnMouseDownClickCount(object sender, MouseEventArgs ae)
+        {
+
+            int n= ae.Clicks;
+        }
+    }
+}
